5b95b114c60dc2f6d15791dca94c070b95b7dae2
Yogeswar vemula <yogeswar6767@gmail.com>
2026-08-31 15:55:43 +0530

    Add files via upload
    arthemetic operators excersice python code

---
+++ operators.py
@@ -0,0 +1,8 @@
+#python comparision operators excersice 
+a = 61246437
+b = 61246437
+result = a > b #greater than comparision operator
+print("result of",a, ">",b, "is : ", result)
+
+result = a < b # Less than comparision operator 
+print("result of",a, "<",b, "is : ", result)
