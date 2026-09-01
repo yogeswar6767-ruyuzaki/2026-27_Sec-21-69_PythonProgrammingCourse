3bd01d708e51b236212e4f8122f39db337be2b7c
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-01 13:56:33 +0530

    Add files via upload
    identity operators with both cases

---
+++ CLASS.py
@@ -0,0 +1,8 @@
+#identity operator example TRUE
+a = [1, 2 , 3]
+b = a
+print("result of",a,"is", a is b)
+#identity operator example FALSE
+a = [1, 2, 3]
+b = [1, 2, 3]
+print("result of",a,"is", a is b)
