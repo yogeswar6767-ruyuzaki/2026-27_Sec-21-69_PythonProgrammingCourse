0167bfe8a8718073310cd7fceb135fd454479dec
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-04 14:21:43 +0530

    Add files via upload
    membership operator excercise

---
+++ membership operator.py
@@ -0,0 +1,12 @@
+# membership operator exercise 
+'a' in 'apple' 
+print("result of 'a' in 'apple' is : ", 'a' in 'apple') # answer is true because 'a' is present in 'apple'
+
+'x' not in 'apple' 
+print("result of 'x' not in 'apple' is : ", 'x' not in 'apple') # answer is true because 'x' is not present in 'apple'
+
+'1' in [3,4,5,6]
+print("result of '1' in [3,4,5,6] is : ", '1' in [3,4,5,6]) # answer is false because '1' is not present in the list [3,4,5,6]
+
+5 not in [3,4,5,6]
+print("result of 5 not in [3,4,5,6] is : ", 5 not in [3,4,5,6]) # answer is false because '5' is present in the list [3,4,5,6]
