b7efaae60345ebac16a5b0115ce633d2c3ea1a0e
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-04 15:50:54 +0530

    Add files via upload
    simple interest excersice

---
+++ labtask6.py
@@ -0,0 +1,6 @@
+# calculating simple interest 
+p = float(input("Enter the principal amount: ")) # in rupees 
+r = float(input("Enter the rate of interest: ")) # in percentage 
+t = float(input("Enter the time period: ")) # in years 
+SI = (p * r * t) / 100
+print("The simple interest is: ", SI) 
