937ffe37ddcb6fe0669d3f3948a05fab4a74b32c
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-08 13:25:43 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,22 @@
+#python logical operator excercise AND false
+a = 5
+result = a > 8 and a < 9
+print("result of",a, ">8 and <9 is :",result)
+
+#python logical operator excercise AND true
+
+a = 5 
+result = a > 3 and a < 7
+print("result of",a, ">3 and <7 is :",result)
+
+#python logical operator excercise OR true
+
+a = 6
+result = a > 5 and a <10
+print("result of",a, ">5 and <1o is :",result)
+
+#python logical operator excercise OR false
+
+a = 6
+result = a > 7 and a <4
+print("result of",a, ">7 and <4 is :",result)
