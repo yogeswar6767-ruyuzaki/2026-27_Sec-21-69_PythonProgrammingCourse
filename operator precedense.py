918e870bec2f51408215064b1ef9b7cec1e0e704
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-18 13:40:13 +0530

    Add files via upload

---
+++ operator precedense.py
@@ -0,0 +1,13 @@
+# program to demonstrate operator precedence and associativity exercise
+
+a = float(input("Enter a number : 1 : "))
+
+b = float(input("Enter a number : 2 : "))
+
+c = float(input("Enter a number : 3 : "))
+
+d = float(input("Enter a number : 4 : "))
+
+result = a + b * c ** d
+
+print("result of", a, "+", b, "*", c, "**", d, "is :", result)
