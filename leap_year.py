8afd352790ed2bb402adfdf142ddd0d26fd7c78c
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-25 16:11:22 +0530

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,6 @@
+# python program to check whether a given year is a leap year
+year = int(input("Enter a year: "))
+if year % 4 == 0:
+    print(f"{year} is a leap year") 
+else:
+    print(f"{year} is not a leap year")
