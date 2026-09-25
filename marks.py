1fa861f3d6ceeb12d6d630d641175aad71d5f9b3
Yogeswar vemula <yogeswar6767@gmail.com>
2026-09-25 16:00:05 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter your marks: "))
+if marks >=90:
+    print(f"grade : A , marks : {marks}")
+elif marks >=80:
+    print(f"grade : B , marks : {marks}")
+elif marks >=70:
+    print(f"grade : C , marks : {marks}")
+else:
+    print(f"grade : D , marks : {marks}")
